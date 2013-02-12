b061ca6234b70a75d229bc0879a9634da6295553
priya <priya11051988@gmail.com>
2013-02-12 12:07:13 +0530

    simplified the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 DemoApp::Application.routes.draw do
   resources :users
+  root :to => 'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +51,7 @@ DemoApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # 
 
   # See how all your routes lay out with "rake routes"
 
