79abf1b89c94e54d2f536ddba67a579f7f556d99
priya <priya11051988@gmail.com>
2013-02-20 15:20:44 +0530

    Initial commit

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
   validates :content, :length => {:maximum => 140}
+  validates_numericality_of :user_id
   validates_presence_of :content
   validates_presence_of :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
   has_many :microposts
+  validates :name, :length => {:maximum => 12}
+  validates_presence_of :email
+  validates_presence_of :name
 end
